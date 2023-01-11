796a0d1b7a53b29be5bfea9f64d18818ad75f676
Kowenjko <74559496+Kowenjko@users.noreply.github.com>
2023-01-11 12:26:52 +0200

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createMemoryHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LeafletView from '../views/LeafletView.vue'
 
 const router = createRouter({
-  history: createMemoryHistory('/vue-task-mgi/'),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
